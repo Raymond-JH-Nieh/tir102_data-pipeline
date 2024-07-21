f017656eafde41b98220249e2796909733f55267
Raymond <rayey11841@gmail.com>
2024-07-21 16:53:27 +0800

    practice: ptt_flow build up in class

---
+++ src/pandas_practice/ptt_flow.py
@@ -0,0 +1,44 @@
+import glob
+
+import pandas as pd
+
+#Define functions
+def get_text_file_paths() -> list[str]:
+    return glob.glob(
+        "/workspaces/tir102_data-pipeline/res_gossiping/*.txt"
+    )
+
+def e_text_file(path: str) -> str:
+    with open(path, "r", encoding = "utf-8") as f:
+        return f.read()
+    
+def t_text_to_df_row_list(reply_info_string: str) -> list[str]:
+    reply_info_string = article_string.split("---split---")[1]
+    return reply_info_string.split("\n")[1:-1]
+
+def t_combine_list_to_df(reply_info_list: list[list]) -> pd.DataFrame:
+    return pd.DataFrame(
+        data = reply_info_lists,
+        cloumns = COLUMNS,
+    )
+
+def l_df_to_csv(df: pd.DataFrame) -> None:
+    df.to_csv("ptt.csv", index=0)
+
+if __name__ == "__main__": #主要函數會在main裡頭
+    pass
+#Get path of all text files
+path_list = get_text_file_paths()
+
+#Loop for file paths
+for path in paths_list: 
+    #Extract text file
+    article_string = e_text_file(path)
+    
+    #Text to list-element in DataFrame
+    reply_info_list = t
+    #Concat lists to DataFrame
+    
+#Load DataFrame to CSV
+
+    
